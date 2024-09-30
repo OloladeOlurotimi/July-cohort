aa96f15271ffaf182d178d3496b442eaf5d2d9a9
unknown <ololadebamidele91@gmail.com>
2024-09-30 11:45:11 +0100

    computer is working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -45,8 +46,121 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> {
   int player = 1;
   int computer = 2;
+  bool isComputerTurn = false;
   List board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  Timer? _timer;
+  int time = 60;
+  String status = 'Your turn';
+  startTime() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (time > 0) {
+        setState(() {
+          time--;
+        });
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  runComputer() async {
+    await Future.delayed(Duration(milliseconds: 200), () {
+      int? blocking;
+      int? winning;
+      int? normal;
+
+      for (int i = 0; i < board.length; i++) {
+        List demoBoard = List.from(board);
+        if (demoBoard[i] > 0) {
+          continue;
+        }
+
+        demoBoard[i] = computer;
+        if (hasWon(computer, demoBoard)) {
+          winning = i;
+        }
+        demoBoard[i] = player;
+
+        if (hasWon(player, demoBoard)) {
+          blocking = i;
+          //log(i.toString());
+        }
+        normal = i;
+      }
+
+      int move = winning ?? blocking ?? normal ?? 0;
+
+      makeMove(computer, move);
+
+      setState(() {
+        board[move] = computer;
+      });
+      if (hasWon(player, board)) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('You Won'),
+          ),
+        );
+      }
+    });
+  }
+
+  void makeMove(int check, int index) {
+    print('playing $index');
+    setState(() {
+      board[index] = check;
+    });
+  }
+
+  bool hasWon(int player, List board) {
+    //012
+    //345
+    //678
+    //036
+    //147
+    //258
+    //048
+    //246
+
+    return board[0] == player && board[1] == player && board[2] == player ||
+        board[4] == player && board[5] == player ||
+        board[6] == player && board[7] == player && board[8] == player ||
+        board[0] == player && board[1] == player && board[2] == player ||
+        board[3] == player && board[4] == player && board[5] == player ||
+        board[6] == player && board[7] == player && board[8] == player ||
+        board[0] == player && board[3] == player && board[6] == player ||
+        board[1] == player && board[4] == player && board[7] == player ||
+        board[2] == player && board[5] == player && board[8] == player ||
+        board[0] == player && board[4] == player && board[8] == player ||
+        board[2] == player && board[4] == player && board[6] == player;
+  }
+
+//Async
+  //Future
+  //Stream
+  /* Future greetUser() async {
+    Future.delayed(Duration(seconds: 2), () {
+      print('Am Delay 2 seconds');
+    });
+
+    print('I will print in the second position');
+  }
+
+  Future oneTo100() async {
+    for (int i = 0; i < 60; i++) {
+      await Future.delayed(Duration(seconds: 1), () {
+        print(i);
+      });
+    }
+  }*/
+
   @override
+  void initState() {
+    startTime();
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -54,19 +168,34 @@ class _GameScreenState extends State<GameScreen> {
         leading: Container(
           alignment: Alignment.center,
           child: Text(
-            '60',
+            '$time',
             style: TextStyle(fontSize: 20),
           ),
         ),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.pause)),
           IconButton(
               onPressed: () {
+                if (_timer != null && !_timer!.isActive) {
+                  startTime();
+                } else {
+                  setState(() {
+                    _timer?.cancel();
+                  });
+                }
+              },
+              icon: Icon(_timer != null && _timer!.isActive
+                  ? Icons.pause
+                  : Icons.play_arrow)),
+          IconButton(
+              onPressed: () {
+                _timer?.cancel();
                 setState(() {
                   board = List.filled(9, 0);
+                  time = 60;
                 });
+                startTime();
               },
-              icon: Icon(Icons.lock_reset))
+              icon: const Icon(Icons.lock_reset_rounded))
         ],
       ),
       body: Column(
@@ -82,10 +211,19 @@ class _GameScreenState extends State<GameScreen> {
                   children: [
                     for (int i = 0; i < 9; i++)
                       GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            board[i] = player;
-                          });
+                        onTap: () async {
+                          if (!isComputerTurn) {
+                            setState(() {
+                              board[i] = player;
+                              isComputerTurn = true;
+                              status = 'Computer Turn';
+                            });
+                            await runComputer();
+                            setState(() {
+                              isComputerTurn = false;
+                              status = 'Your Turn';
+                            });
+                          }
                         },
                         child: Container(
                           alignment: Alignment.center,
@@ -110,7 +248,13 @@ class _GameScreenState extends State<GameScreen> {
                           ),
                         ),
                       )
-                  ]))
+                  ])),
+          Text(
+            status,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
+          )
         ],
       ),
     );
